c2f70e9cfd37c5f6dd68df99ddbfb15ca2d55383
amir <amirsdream@gmail.com>
2020-01-12 17:48:07 +0300

    new values

--- filebeat.sh
+++ filebeat.sh
@@ -1,5 +1,6 @@
 docker run -d --privileged\
   --name=filebeat \
+  --user=root \
   --volume="`pwd`/config/filebeat.yml:/usr/share/filebeat/filebeat.yml:ro" \
   --volume="/var/run/docker.sock:/var/run/docker.sock:ro" \
   --volume="/var/lib/docker/containers:/var/lib/docker/containers:ro" \
@@ -7,4 +8,4 @@ docker run -d --privileged\
   --restart=always \
   docker.elastic.co/beats/filebeat:7.4.2 filebeat -e -strict.perms=false \
   -E output.elasticsearch.hosts=["elasticsearch:9200"] \
-  -E setup.kibana.host=kibana:5601
+  -E setup.kibana.host=http://kibana:5601
--- metricbeat.sh
+++ metricbeat.sh
@@ -1,5 +1,6 @@
 docker run -d \
   --name=metricbeat \
+  --user=root \
   --volume="`pwd`/config/metricbeat.yml:/usr/share/metricbeat/metricbeat.yml:ro" \
   --volume="/var/run/docker.sock:/var/run/docker.sock:ro" \
   --volume="/sys/fs/cgroup:/hostfs/sys/fs/cgroup:ro" \
@@ -7,6 +8,6 @@ docker run -d \
   --volume="/:/hostfs:ro" \
   --net=elk \
   --restart=always \
-  docker.elastic.co/beats/metricbeat:7.4.2 metricbeat -e \
+  docker.elastic.co/beats/metricbeat:7.4.2 metricbeat -e -strict.perms=false\
   -E output.elasticsearch.hosts=["elasticsearch:9200"] \
   -E setup.kibana.host=kibana:5601
